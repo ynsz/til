b51d83feadd5e9a52aab76eb99801c39486a2b3f
ynsz <99311398+ynsz@users.noreply.github.com>
2025-07-26 15:30:18 +0900

    Tailwind CSSを学んだよ

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,12 +1,18 @@
-// src/App.jsx
-import { Emotion } from './components/Emotion';
+import { CssModules } from "./components/CssModules";
+import { Emotion } from "./components/Emotion";
+import { StyledComponents } from "./components/StyledComponents";
+import { StyledJsx } from "./components/StyledJsx";
+import { TailwindCss } from "./components/TailwindCss";
 
-function App() {
+export const App = () => {
   return (
-    <>
+    <div>
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
       <Emotion />
-    </>
-  )
-}
-
+      <TailwindCss />
+    </div>
+  );
+};
 export default App
